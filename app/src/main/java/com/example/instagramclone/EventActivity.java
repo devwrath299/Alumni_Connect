fdb39279cf75d938f39f26d3c7aa3127e2871650
Dev <devwrath1999@gmail.com>
2023-02-10 15:55:30 +0530

    Spinner

--- app/src/main/java/com/example/instagramclone/EventActivity.java
+++ app/src/main/java/com/example/instagramclone/EventActivity.java
@@ -2,10 +2,15 @@ package com.example.instagramclone;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.instagramclone.model.College;
+import com.example.instagramclone.model.CollegeAdapter;
+import com.example.instagramclone.model.Post;
+import com.example.instagramclone.model.PostAdapter;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
@@ -16,12 +21,27 @@ import java.util.List;
 
 public class EventActivity extends AppCompatActivity {
     List<College>collegeList=new ArrayList<>();
+    RecyclerView recycler_view;
+    CollegeAdapter clgAdapter;
+    List<College>list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
+
+        list=new ArrayList<>();
+        recycler_view=findViewById(R.id.collegeRecyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+        linearLayoutManager.setStackFromEnd(true);
+        linearLayoutManager.setReverseLayout(true);
+
+        recycler_view.setLayoutManager(linearLayoutManager);
+        clgAdapter = new CollegeAdapter(getApplicationContext(),list);
+        recycler_view.setAdapter(clgAdapter);
         fetchColleges();
+
+
     }
 
     private void fetchColleges() {
@@ -32,6 +52,7 @@ public class EventActivity extends AppCompatActivity {
                 for (DataSnapshot np : snapshot.getChildren()) {
                     collegeList.add(np.getValue(College.class));
                 }
+                clgAdapter.notifyDataSetChanged();
             }
 
             @Override
